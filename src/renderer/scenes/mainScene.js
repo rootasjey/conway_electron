687cea5c43a1bb762f34bfcba826ded6242d0936
rootasjey <jeremiecorpinot@outlook.com>
2018-11-04 17:31:06 +0100

    Add edition mode (when paused)

--- src/renderer/scenes/mainScene.js
+++ src/renderer/scenes/mainScene.js
@@ -11,6 +11,7 @@ export default class MainScene extends Phaser.Scene {
       colors: {
         cellBorder: 0x78e08f,
         cell: 0xb8e994,
+        editionMode: { add: 0xb8e994, remove: 0xED4C67 },
         grid: 0xecf0f1,
       },
       dimensions: {
@@ -32,11 +33,14 @@ export default class MainScene extends Phaser.Scene {
     this.ui = {
       buttons: {
         pause: {},
+        editionMode: {},
         play: {},
         restart: {},
       },
       cells: {},
       counter: {},
+      editionMode: 'add',
+      pointer: {},
     }
 
     return this
@@ -45,14 +49,71 @@ export default class MainScene extends Phaser.Scene {
   create() {
     const { columns, rows } = this.config.dimensions
 
-    this.drawGrid({ columns: columns, rows: rows})
+    this.createGrid({ columns: columns, rows: rows, debug: true })
       .createInitialState()
       .createCounter()
       .createUIControls()
+      .createPointer()
       .listenEvents()
   }
 
-  createUIControls() {
+  createEditionModeButton() {
+    const { innerWidth: x } = window
+    const y = 190
+    const color = 0xFFC312
+
+    // Pause button
+    const r1 = this.add.rectangle(0, 0, 10, 50, color)
+    const r2 = this.add.rectangle(0, 0, 50, 10, color)
+
+    this.ui.buttons.editionMode = this.add
+      .container(x - 50, y, [r1, r2])
+      .setSize(50, 100)
+      .setInteractive()
+      .setAlpha(.8)
+      .setDepth(2)
+
+    // Events
+    const { editionMode } = this.ui.buttons
+
+    editionMode.once('destroy', () => {
+      r1.destroy()
+      r2.destroy()
+    })
+
+    editionMode.on('pointerdown', () => {
+      const { paused } = this.state
+
+      if (paused) {
+        const { add: colorAdd, remove: colorRemove }
+          = this.config.colors.editionMode
+
+        this.ui.editionMode = this.ui.editionMode === 'add' ?
+          'remove' : 'add'
+
+        if (this.ui.editionMode === 'add') {
+          r1.setVisible(true)
+          this.ui.pointer.setFillStyle(colorAdd)
+
+        } else {
+          r1.setVisible(false)
+          this.ui.pointer.setFillStyle(colorRemove)
+        }
+      }
+    })
+
+    editionMode.on('pointerover', () => {
+      editionMode.setAlpha(1)
+    })
+
+    editionMode.on('pointerout', () => {
+      editionMode.setAlpha(.8)
+    })
+
+    return this
+  }
+
+  createPauseButton() {
     const { innerWidth: x } = window
     const y = 40
 
@@ -64,15 +125,81 @@ export default class MainScene extends Phaser.Scene {
       .container(x - 60, y, [r1, r2])
       .setSize(50, 100)
       .setInteractive()
-      .setAlpha(.5)
+      .setAlpha(.8)
       .setVisible(false)
+      .setDepth(2)
+
+    // Events
+    const { pause } = this.ui.buttons
+
+    pause.once('destroy', () => {
+      r1.destroy()
+      r2.destroy()
+    })
+
+    pause.on('pointerdown', () => {
+      const { paused } = this.state
+      const { play } = this.ui.buttons
+
+      if (!paused) {
+        this.pause()
+        play.setVisible(true)
+        pause.setVisible(false)
+      }
+    })
+
+    pause.on('pointerover', () => {
+      pause.setAlpha(1)
+    })
+
+    pause.on('pointerout', () => {
+      pause.setAlpha(.8)
+    })
+
+    return this
+  }
+
+  createPlayButton() {
+    const { innerWidth: x } = window
+    const y = 40
 
     // Play button
     this.ui.buttons.play = this.add.triangle(
       x - 45, y, 0, 0, 0, 45, 45, 22.5, 0xe84118)
       .setInteractive()
-      .setAlpha(.5)
+      .setAlpha(.8)
+      .setDepth(2)
 
+    // Events
+    const { play, pause } = this.ui.buttons
+    // const { editionMode } = this.ui.buttons
+
+    play.on('pointerdown', () => {
+      const { paused } = this.state
+
+      if (paused) {
+
+        this.start()
+
+        play.setVisible(false)
+        pause.setVisible(true)
+      }
+    })
+
+    play.on('pointerover', () => {
+      play.setAlpha(1)
+    })
+
+    play.on('pointerout', () => {
+      play.setAlpha(.8)
+    })
+
+    return this
+  }
+
+  createRestartButton() {
+    const { innerWidth: x } = window
+    const y = 40
 
     // Restart button
     var circle = this.add.circle(0, 10, 20)
@@ -90,65 +217,20 @@ export default class MainScene extends Phaser.Scene {
       )
       .setSize(50, 60)
       .setInteractive()
-      .setAlpha(.5)
+      .setAlpha(.8)
+      .setDepth(2)
 
     // Events
-    // ------
-    const { pause, play, restart } = this.ui.buttons
-
-    // destroy
-    pause.once('destroy', () => {
-      r1.destroy()
-      r2.destroy()
-    })
+    const { restart } = this.ui.buttons
 
     restart.once('destroy', () => {
       circle.destroy()
       triangle.destroy()
     })
 
-    // Pause
-    pause.on('pointerdown', () => {
-      const { paused } = this.state
-
-      if (!paused) {
-        this.pause()
-        play.setVisible(true)
-        pause.setVisible(false)
-      }
-    })
-
-    pause.on('pointerover', () => {
-      pause.setAlpha(1)
-    })
-
-    pause.on('pointerout', () => {
-      pause.setAlpha(.5)
-    })
-
-    // Play
-    play.on('pointerdown', () => {
-      const { paused } = this.state
-
-      if (paused) {
-        this.start()
-
-        play.setVisible(false)
-        pause.setVisible(true)
-      }
-    })
-
-    play.on('pointerover', () => {
-      play.setAlpha(1)
-    })
-
-    play.on('pointerout', () => {
-      play.setAlpha(.5)
-    })
-
-    // Restart
-    restart.on('pointerdown', () => {
+    restart.on('pointerup', (pointer, x, y, options) => {
       this.restart()
+      options.stopPropagation()
     })
 
     restart.on('pointerover', () => {
@@ -156,8 +238,18 @@ export default class MainScene extends Phaser.Scene {
     })
 
     restart.on('pointerout', () => {
-      restart.setAlpha(.5)
+      restart.setAlpha(.8)
     })
+
+    return this
+  }
+
+  createUIControls() {
+    this
+    .createPauseButton()
+    .createPlayButton()
+    .createRestartButton()
+    .createEditionModeButton()
 
     return this
   }
@@ -169,7 +261,8 @@ export default class MainScene extends Phaser.Scene {
       `${step}`, {
         fontFamily: 'Arial', fontSize: 64, color: '#fbc531' })
         .setInteractive()
-        .setAlpha(.6);
+        .setAlpha(.6)
+        .setDepth(2);
 
     const { counter } = this.ui
 
@@ -241,7 +334,7 @@ export default class MainScene extends Phaser.Scene {
   /**
    * Create grid
    */
-  drawGrid({ columns = 80, rows = 40 }) {
+  createGrid({ columns = 80, rows = 40, debug = false }) {
     const { grid: color } = this.config.colors
 
     const rowSize = window.innerHeight / rows;
@@ -252,21 +345,34 @@ export default class MainScene extends Phaser.Scene {
 
     this.config.cellSize = { h: rowSize, w: columnSize };
 
-    for (let index = 0; index < columns; index++) {
-      const columnOffset = columnSize * index;
-      this.add.line(0, 0, columnOffset, 0, columnOffset, columnHeight, color);
-    }
+    if (debug) {
+      for (let index = 0; index < columns; index++) {
+        const columnOffset = columnSize * index;
+        this.add.line(0, 0, columnOffset, 0, columnOffset, columnHeight, color);
+      }
 
-    for (let index = 0; index < rows; index++) {
-      const rowOffset = rowSize * index;
-      this.add.line(0, 0, 0, rowOffset, rowWidth, rowOffset, color);
+      for (let index = 0; index < rows; index++) {
+        const rowOffset = rowSize * index;
+        this.add.line(0, 0, 0, rowOffset, rowWidth, rowOffset, color);
+      }
     }
 
     return this;
   }
 
+  createPointer() {
+    const { h, w } = this.config.cellSize
+    const { cell: color } = this.config.colors
+
+    this.ui.pointer = this.add.rectangle(100, 20, w, h, color)
+      .setVisible(false)
+      .setDepth(1)
+
+    return this
+  }
+
   freeMemory() {
-    for (const [, button] of Object.entries(this.ui.buttons)) {
+    for (const [key, button] of Object.entries(this.ui.buttons)) {
       button.destroy()
     }
 
@@ -275,10 +381,15 @@ export default class MainScene extends Phaser.Scene {
     }
 
     this.ui.counter.destroy()
+    this.ui.pointer.destroy()
+
+    this.ui.pointer = undefined
 
     window.clearInterval(this.timer)
 
     ipcRenderer.removeAllListeners('tick-reply')
+    this.input.removeAllListeners('pointermove')
+    this.input.removeAllListeners('pointerup')
 
     return this
   }
@@ -308,6 +419,85 @@ export default class MainScene extends Phaser.Scene {
   listenEvents() {
     ipcRenderer.on('tick-reply', (event, data) => {
       this.renderState(data)
+    })
+
+    this.input.on('pointermove', (pointer) => {
+      if (!this.state.paused) { return; }
+      const { x, y } = pointer
+      const { h, w } = this.config.cellSize
+
+      if (!this.ui.pointer.visible) {
+        this.ui.pointer.setVisible(true)
+      }
+
+      const cellX = Math.floor(x / w)
+      const cellY = Math.floor(y / h)
+
+      const newX = (cellX + .5) * w
+      const newY = (cellY + .5) * h
+
+      this.ui.pointer.x = newX
+      this.ui.pointer.y = newY
+
+      const { cell: color } = this.config.colors
+
+      const key = `${cellX},${cellY}`
+
+      if (pointer.isDown) {
+        const { cells: visualCells, editionMode } = this.ui
+        const { cells } = this.state
+
+        if (editionMode === 'add') {
+          if (cells[key]) { return; }
+
+          visualCells[key] = this.add.rectangle(newX, newY, w, h, color)
+          cells[key] = { x: cellX, y: cellY }
+
+        } else {
+          if (!cells[key]) { return; }
+
+          visualCells[key].destroy()
+          cells[key] = undefined
+
+          delete visualCells[key]
+          delete cells[key]
+        }
+
+      }
+    })
+
+    this.input.on('pointerup', () => {
+      if (!this.state.paused) { return; }
+
+      const { cells } = this.state
+      const { cells: visualCells, editionMode } = this.ui
+
+      const { h, w } = this.config.cellSize
+      const { cell: color } = this.config.colors
+      const { x, y } = this.ui.pointer
+
+
+      const cellX = Math.round((x / w) - .5)
+      const cellY = Math.round((y / h) - .5)
+
+
+      const key = `${cellX},${cellY}`
+
+      if (editionMode === 'add') {
+        if (cells[key]) { return; }
+
+        visualCells[key] = this.add.rectangle(x, y, w, h, color)
+        cells[key] = { x: cellX, y: cellY }
+
+      } else {
+        if (!cells[key]) { return; }
+
+        visualCells[key].destroy()
+        cells[key] = undefined
+
+        delete visualCells[key]
+        delete cells[key]
+      }
     })
 
     return this
@@ -359,6 +549,7 @@ export default class MainScene extends Phaser.Scene {
   start() {
     this.timer = window.setInterval(() => { this.tick() }, 1000)
     this.state.paused = false
+    this.ui.pointer.setVisible(false)
 
     return this;
   }
